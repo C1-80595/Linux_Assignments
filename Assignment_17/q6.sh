4ee54bda168de38ac0c39f425bc382ec2b0854dd
C1-80595 <rupalturkane37@gmail.com>
2023-10-23 23:01:28 +0530

    Assignment_17

---
+++ Assignment_17/q6.sh
@@ -0,0 +1,32 @@
+#!/bin/bash
+
+#Write shell Script which take number from user & print following lines Enter number of lines : 5
+
+echo -n "Enter the number of rows: "
+read num_rows
+
+if ! [[ "$num_rows" =~ ^[0-9]+$ ]];
+then
+    echo "Invalid input. Please enter a valid number."
+    exit 1
+fi
+
+for ((i = 1; i <= num_rows; i++));
+do
+    for ((j = 1; j <= num_rows - i; j++));
+    do
+        echo -n "  "
+    done
+
+    for ((k = 1; k <= i; k++));
+    do
+        echo -n "$k "
+    done
+
+    for ((l = i - 1; l >= 1; l--));
+    do
+        echo -n "$l "
+   done
+   echo
+done
+
